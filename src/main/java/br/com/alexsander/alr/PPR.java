82a84a6b58827ceb91a696d41502ed20b9dae36a
Tailine Wornath <taywornath>
2020-11-16 18:36:49 -0300

    Update PPR.java

--- src/main/java/br/com/alexsander/alr/PPR.java
+++ src/main/java/br/com/alexsander/alr/PPR.java
@@ -1,9 +1,13 @@
 package br.com.alexsander.alr;
 
+import br.com.alexsander.alr.TipoToken;
+
 /**
  *
  * @author alexs
+ * @author taywornath
  */
+
 public class PPR extends Parser {
 
     public PPR(String arquivo) {
@@ -17,13 +21,46 @@ public class PPR extends Parser {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    public boolean analisaPrograma() {
-        boolean bandeira = true;
-        
+    public boolean analisaPrograma() throws IOException{
+        /*boolean bandeira = true;
         if(erro(" "))
             bandeira = false;
-        
         return bandeira;
+        */
+    	
+    	buscaToken();
+    	
+    	if (t.tipo == TipoToken.SPROGRAMA) {
+    		System.out.print (token.tipo + "");
+    		buscaToken();
+    		
+    		if (t.tipo == TipoToken.SIDENTIFICADOR) {
+    			System.out.print(t.tipo +": " + t.c + ' ');
+    			buscaToken();
+    			//Adiciona identificador na tabela de simbolos
+    			ts.ts.put(c, t);
+    			buscaToken();
+    			
+    			if (t.tipo == TipoToken.SPONTO_E_VIRGULA) {
+    				System.out.print(t.tipo + " ");
+    				if (analisaBloco()) {
+    					buscaToken();
+    					if (t.tipo == TipoToken.SPONTO)
+    						System.out.print(T.tipo + " ");
+    						return true;
+    				}else {
+    					System.out.println("Bloco principal nao encontrado: " +t.linha +", " +t.coluna);
+    					return false;
+    				}
+    			}else {
+    				System.out.println("Ponto e virgula esperado: " +t.linha +", " +t.coluna);
+					return false;
+    			}
+    		}
+    	}else {
+    		System.out.println("Identificador esperado: " +t.linha +", " +t.coluna);
+			return false;
+    	}
     }
     
     public boolean analisaBloco() {
